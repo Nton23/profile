0deef7af567525d081b93ecf893a4db0bd404b9f
Nton23 <huutonau@gmail.com>
2023-06-15 20:52:03 -0700

    Add custom screen sizes and font styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        regStyle: ['Domine', 'sans-serif'],
+        headingName: ['Lilita One', 'serif'],
+        bttnStyle: ['Teko', 'monospace'],
+      }
+    },
+    screens: {
+      'sm': '480px',
+      'md': '768px',
+      'lg': '1020px',
+      'xl': '1440px',
+      '2xl': '2160px',
+      '3xl': '2700px'
+    }
   },
   plugins: [],
 }
